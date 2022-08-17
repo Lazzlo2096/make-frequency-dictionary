762849b99bda528d110dd31c7e41de6188a41ebe
Lazzlo2096 <lazzlo2096@yandex.ru>
2022-08-17 23:15:46 +0300

    ctime to chrono; some optim;

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+// g++ ./main.cpp -std=c++2a
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
+#include <chrono>
 // #include <time.h>
 
 #include <set>
@@ -14,21 +16,26 @@
 
 //---------------------------------
 // https://github.com/Lazzlo2096/Zeitgeist/blob/master/utilities.cpp
-double getTimeInMillis()
+/*double getTimeInMillis()
 {
 	clock_t now = clock();
 	return 1000.0*now / CLOCKS_PER_SEC;
-}
+}*/
 
-std::vector<std::string> getWordList(const char* filename)
+inline std::vector<std::string> getWordList(const char* filename)
 {
 	//First, read in the file
 	// I'm not doing much error checking here, because I am a bad person
 	std::ifstream in(filename);
 	std::string s;
 	std::vector<std::string> wordlist;
-	while (in >> s){
-		wordlist.push_back(s);
+	while (in >> s)
+	{
+		wordlist.push_back(std::move(s));
+		//s.clear();
+
+		// Если будет некорректно? работать, то можно сделать s.clear() после мува, чтобы вернуть строку в детерменированное состояние
+		// Просто после перемещения строка будет в хз каком виде, и я не знаю, как operator>> это обработает и обработает ли
 	}
 
 	return wordlist;
@@ -67,6 +74,19 @@ std::vector<std::string> split(std::string text)
 
 int main()
 {
+
+	uint64_t countThem_i = 0;
+	uint64_t textToWords_i = 0; 
+	uint64_t end_i = 0;
+	std::chrono::time_point<std::chrono::system_clock> start_0, start, textToWords, countThem, end;
+	int n = 500;
+
+	start_0 = std::chrono::system_clock::now();
+
+for (int i = 0; i < n; ++i)
+{
+
+
 	std::map<std::string, int> m;
 
 /*
@@ -75,38 +95,46 @@ first entering a neighbourhood, this truth is so well fixed in the minds\
 of the surrounding families, that he is considered the rightful property\
 of some one or other of their daughters.";
 */
-	double start = getTimeInMillis();
+	///double start = getTimeInMillis();
+	
+	start = std::chrono::system_clock::now();
 
 	//std::vector<std::string> words = split(text);
 	std::vector<std::string> words = getWordList("./data/28885.txt");
 
 	// ----------
 
-	double textToWords = getTimeInMillis();
+	//double textToWords = getTimeInMillis();
+	textToWords = std::chrono::system_clock::now();
+	textToWords_i += std::chrono::duration_cast<std::chrono::milliseconds>( textToWords - start ).count();
 
 	// ----------
-	for (auto word : words)
+	for (const auto &word : words)
 	{
-		if ( m.contains(word) ) // ?? only m[word]++ ?
+		//if ( m.contains(word) ) //[[likely]]
 			m[word]++;
-		else
-			m[word] = 1;
+		//else
+		//	m[word] = 1;
 
-		// std::cout << word << " - " << m[word] << std::endl;
+		//std::cout << word << " - " << m[word] << std::endl;
 	}
 	// ----------
 
-	double countThem = getTimeInMillis();
+	//double countThem = getTimeInMillis();
+	countThem = std::chrono::system_clock::now();
+	countThem_i += std::chrono::duration_cast<std::chrono::milliseconds>( countThem - textToWords ).count();
+
 
 	// ---------- sort by value
 	// https://stackoverflow.com/a/55401806
 	typedef std::pair<int, std::string> PAIR;
 	std::set<PAIR, std::greater<PAIR> > s;  // The new (temporary) container.
 
-	for (auto const &kv : m)
-		s.emplace(kv.second, kv.first); // Flip the pairs.
+	for (auto &kv : m)
+		s.emplace(std::move(kv.second), std::move(kv.first)); // Flip the pairs.
 
 
+	/*
 	int n = 10;
 	for (auto const &vk : s)
 	{
@@ -117,17 +145,49 @@ of some one or other of their daughters.";
 
 		//std::cout << vk.first << " " << vk.second << std::endl;
 	}
+	*/
+
 	// ----------
 
-	double end = getTimeInMillis();
+	//double end = getTimeInMillis();
+	end = std::chrono::system_clock::now();
+	end_i += std::chrono::duration_cast<std::chrono::milliseconds>( end - countThem ).count();
+
+}
 
 	//=====================
 
-	std::cout << "start: " << start << std::endl;
-	std::cout << "textToWords: " << textToWords << " [" <<  (textToWords-start) << "] " << std::endl;
-	std::cout << "countThem: " << countThem << " [" <<  (countThem-textToWords) << "] " << std::endl;
-	std::cout << "end(sortThem): " << end << " [" <<  (end-countThem) << "] " << std::endl;
+	//std::cout << "start: " << start << std::endl;
+	//std::cout << "textToWords: " << textToWords << " [" <<  (textToWords-start) << "] " << std::endl;
+	//std::cout << "countThem: " << countThem << " [" <<  (countThem-textToWords) << "] " << std::endl;
+	//std::cout << "end(sortThem): " << end << " [" <<  (end-countThem) << "] " << std::endl;
 
+
+	//const auto textToWords_time = std::chrono::duration_cast<std::chrono::milliseconds>(textToWords - start).count();
+	//const auto countThem_time   = std::chrono::duration_cast<std::chrono::milliseconds>(countThem - textToWords).count();
+	//const auto end_el  = std::chrono::duration_cast<std::chrono::milliseconds>(end - countThem).count();
+
+	const auto prog_el  =  std::chrono::duration_cast<std::chrono::seconds>(end - start_0 ).count();
+
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+	std::cout << "finished computation at " << std::ctime(&end_time)
+			  << "elapsed time: " << prog_el << "s\n";
+
+
+
+	long int average_countThem = countThem_i / n;
+	long int average_textToWords = textToWords_i / n;
+	long int average_end = end_i / n;
+
+	//std::cout << "start: " << start << std::endl;
+	std::cout << "textToWords: \t" << " [" <<  average_textToWords << "] " << std::endl;
+	std::cout << "countThem: \t" << " [" <<  average_countThem << "] " << std::endl;
+	std::cout << "end(sortThem): \t" << " [" <<  average_end << "] " << std::endl;
+
+
+
+	
 	
 	return 0;
 }
