392de988c85eff124d260d0a4944a8d032d4cefc
Lazzlo2096 <lazzlo2096@yandex.ru>
2022-08-19 20:39:29 +0300

    Update list of prime numbers.cpp

--- list of prime numbers.cpp
+++ list of prime numbers.cpp
@@ -24,3 +24,33 @@ std::vector<unsigned> prime(unsigned n)
   
 
 }
+
+
+// ---------------------
+
+#include <vector>
+
+std::vector<unsigned> prime(unsigned n)
+{
+  if (n < 2)
+    return {};
+  
+  std::vector<bool> v(n-1, true);
+  
+  //for (int i = 0; i < v.size(); i++)
+  //  v[i] = i+2;
+  
+  for (int i = 2; i <= n; i++)
+    for (int j = i+1; j <= n; j++)
+       if ( j % i == 0 )
+         v[j-2] = false;
+  
+  std::vector<unsigned> res;
+  
+  for (int i = 0; i < v.size(); i++)
+    if (v[i])
+      res.push_back(i+2);
+    
+  return res;
+  
+}
